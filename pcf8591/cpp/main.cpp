fe61d621ff608db1c4bf9aad7fde69d1f79659a4
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2018-06-01 10:08:31 +0200

    add pcf8591 example

---
+++ pcf8591/cpp/main.cpp
@@ -0,0 +1,36 @@
+#include <wiringPi.h>
+#include <pcf8591.h>
+#include <cstdio>
+#include <cstdlib>
+
+
+// PCF8591 default I^2C address
+const int address = 0x48;
+
+const int pinbase = 64;
+
+const int A0 = pinabase + 0;
+const int A1 = pinabase + 1;
+const int A2 = pinabase + 2;
+const int A3 = pinabase + 3;
+
+
+int main(void)
+{
+    int raw_value;
+    float voltage;
+
+    wiringPiSetup();
+    pcf8591Setup(pinbase, address);
+
+    while(true)
+    {
+        // read analog value from pin A0
+        raw_value = analogRead(A0);
+        analogWrite(pinbase, raw_value);
+        voltage = 3.3f * raw_value / 255.0f;
+        printf("ADC value: %d, Voltage: %.2fV\n", raw_value, voltage);
+        delay(100);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ pcf8591/potentiometer/potentiometer.ino
@@ -0,0 +1,44 @@
+#include "Wire.h"
+
+#define PCF8591 (0x90 >> 1) // I2C bus address
+#define AIN0 0x00
+#define AOUT 0x40
+
+int raw_value = 0;
+float voltage = 0.0f;
+int count = 0;
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Wire.beginTransmission(PCF8591); // Start your PCF8591
+  Wire.write(AOUT); // Tell it to output DAC value
+  Wire.write(raw_value);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(PCF8591); // Start your PCF8591
+  Wire.write(AIN0); // Tell it to make an analog measurement
+  Wire.endTransmission();
+  Wire.requestFrom(PCF8591, 1); // Get the measured Data
+  raw_value = Wire.read();
+  if (raw_value < 0)
+  {
+    raw_value = 255;
+  }
+  voltage = (raw_value * 5.0) / 255.0;
+
+  if (count % 5 == 0)
+  {
+    Serial.print("Raw ADC Value = "); Serial.print(raw_value);
+    Serial.print(" Voltage = "); Serial.print(voltage, 3); Serial.println("V");
+    count = 0;
+  }
+  count ++;  
+  
+  delay(100);
+}
