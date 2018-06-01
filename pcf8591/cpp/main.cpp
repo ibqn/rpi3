3c935ac73cf61ab500f06c10fac41772b9ab38bd
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2018-06-01 10:21:32 +0200

    deal with -1 somehow

--- pcf8591/cpp/main.cpp
+++ pcf8591/cpp/main.cpp
@@ -18,6 +18,7 @@ const int A3 = pinbase + 3;
 int main(void)
 {
     int raw_value;
+    int prev_value = 0;
     float voltage;
 
     wiringPiSetup();
@@ -27,6 +28,15 @@ int main(void)
     {
         // read analog value from pin A0
         raw_value = analogRead(A0);
+        // deal with -1 somehow
+        if (raw_value < 0)
+        {
+            raw_value = prev_value;
+        }
+        else
+        {
+            prev_value = raw_value;
+        }
         analogWrite(pinbase, raw_value);
         voltage = 3.3f * raw_value / 255.0f;
         printf("ADC value: %d, Voltage: %.2fV\n", raw_value, voltage);
