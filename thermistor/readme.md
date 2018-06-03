362ad9d35eeb3164e784d2d5c442890119649bfd
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2018-06-03 14:02:07 +0200

    add arduini thermistor example

---
+++ thermistor/readme.md
@@ -0,0 +1,7 @@
+# Thermistor
+
+## Arduino
+
+Below is the hardware connection diagram for a curcuit with a thermistor.
+
+[](thermistor-hardware-connection.png)
---
+++ thermistor/thermistor/thermistor.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("UNO is ready!");
+}
+
+void loop() {
+  int adc_value = analogRead(A0);
+
+  float v = adc_value * 5.0f / 1024;
+  // calculate resistance of thermistor
+  float rt = 10 * v / (5.0f - v);
+  float temp_k = 1 / (log(rt / 10) / 3950 + 1 / (273.15f + 25));
+  float temp_c = temp_k - 273.15f;
+  
+  Serial.print("Current temperature is: ");
+  Serial.print(temp_c);
+  Serial.println(" C");
+  
+  delay(2000);
+}
